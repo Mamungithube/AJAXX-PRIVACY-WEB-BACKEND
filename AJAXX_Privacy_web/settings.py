30e45ec936f35403e6bb6098992d0eb57db82e90
Mamungithube <orroshidmdmamun50@gmail.com>
2025-10-26 01:27:19 +0600

    update ALLOWED_HOSTS to allow all hosts in settings.py

--- AJAXX_Privacy_web/settings.py
+++ AJAXX_Privacy_web/settings.py
@@ -33,8 +33,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
-print("✅ ALLOWED_HOSTS:", ALLOWED_HOSTS)
+ALLOWED_HOSTS = ["*"]
 
 
 AUTH_USER_MODEL = "Account.User"
