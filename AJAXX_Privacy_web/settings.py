54152d3ef5e0930f75abfa01779ad407aa203898
Mamungithube <orroshidmdmamun50@gmail.com>
2026-01-17 21:23:52 +0000

    fasdf

--- AJAXX_Privacy_web/settings.py
+++ AJAXX_Privacy_web/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_celery_results',
+    'django_celery_beat',
     'stripe',
  
     # Local apps
